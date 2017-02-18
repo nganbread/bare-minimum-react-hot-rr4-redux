53853f0ac103ec24a5e4c340e9e107428b362cc1
Scott Ngan <scntt@outlook.com>
2017-02-18 13:36:05 +1300

    Cleaned up store

--- src/store.js
+++ src/store.js
@@ -1,9 +1,12 @@
 import { createStore } from 'redux';
 
-export default createStore((state = 0, action) => {
+const reducer = (state = 0, action) => {
     switch (action.type) {
         case 'INCREMENT':
             return state + 1;
     }
+    
     return state;
-}, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+}
+
+export default createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
